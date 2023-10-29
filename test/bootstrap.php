20d18d15cc9870c519ece34dc68a96502ead1b87
Michele Locati <michele@locati.it>
2023-10-29 16:26:19 +0100

    Set UTF-8 internal encoding

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 use MLUnipoints\Build\FilesystemCache;
 
+mb_regex_encoding('UTF-8');
+ini_set('mbstring.script_encoding', 'pass');
+mb_internal_encoding('UTF-8');
+mb_substitute_character('none');
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 define('MLUNIPOINTS_TEST_PROJECTROOT', rtrim(str_replace(DIRECTORY_SEPARATOR, '/', realpath(__DIR__ . '/..')), '/'));
