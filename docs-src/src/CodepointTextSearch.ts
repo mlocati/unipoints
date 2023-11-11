5a83ea80c20d56e3933ecca7a230d67748c8af16
Michele Locati <michele@locati.it>
2023-11-12 00:20:28 +0100

    Allow searching by regexp (#42)

--- docs-src/src/CodepointTextSearch.ts
+++ docs-src/src/CodepointTextSearch.ts
@@ -1,4 +1,4 @@
-import type { Codepoint } from "./Data"
+import type { Codepoint } from './Data'
 
 interface ICodepointStrings {
   [index: number]: string[]
@@ -6,33 +6,35 @@ interface ICodepointStrings {
 
 const codepointStrings: ICodepointStrings = {}
 
-function buildCodepointUppercaseStrings(codepoint: Codepoint): string[]
-{
-    const result: string[] = [codepoint.name.toUpperCase()];
-    if (codepoint.unicode1Name !== undefined) {
-        result.push(codepoint.unicode1Name.toUpperCase());
-    }
-    codepoint.formalAliases?.forEach((s) => result.push(s.toUpperCase()));
-    codepoint.informativeAliases?.forEach((s) => result.push(s.toUpperCase()));
-    codepoint.correctedNames?.forEach((s) => result.push(s.toUpperCase()));
-    codepoint.controlNames?.forEach((s) => result.push(s.toUpperCase()));
-    codepoint.alternateNames?.forEach((s) => result.push(s.toUpperCase()));
-    codepoint.figments?.forEach((s) => result.push(s.toUpperCase()));
-    codepoint.abbreviations?.forEach((s) => result.push(s.toUpperCase()));
+function buildCodepointUppercaseStrings(codepoint: Codepoint): string[] {
+  const result: string[] = [codepoint.name.toUpperCase()]
+  if (codepoint.unicode1Name !== undefined) {
+    result.push(codepoint.unicode1Name.toUpperCase())
+  }
+  codepoint.formalAliases?.forEach((s) => result.push(s.toUpperCase()))
+  codepoint.informativeAliases?.forEach((s) => result.push(s.toUpperCase()))
+  codepoint.correctedNames?.forEach((s) => result.push(s.toUpperCase()))
+  codepoint.controlNames?.forEach((s) => result.push(s.toUpperCase()))
+  codepoint.alternateNames?.forEach((s) => result.push(s.toUpperCase()))
+  codepoint.figments?.forEach((s) => result.push(s.toUpperCase()))
+  codepoint.abbreviations?.forEach((s) => result.push(s.toUpperCase()))
 
-    return result;
+  return result
 }
 
-function getCodepointUppercaseStrings(codepoint: Codepoint): string[]
-{
-    return codepointStrings[codepoint.id] ?? (codepointStrings[codepoint.id] = buildCodepointUppercaseStrings(codepoint));
+function getCodepointUppercaseStrings(codepoint: Codepoint): string[] {
+  return (
+    codepointStrings[codepoint.id] ??
+    (codepointStrings[codepoint.id] = buildCodepointUppercaseStrings(codepoint))
+  )
 }
 
-export default function match(codepoint: Codepoint, regexOrUppercaseWords: RegExp|string[]): boolean
-{
-    const strings = getCodepointUppercaseStrings(codepoint);
-    if (regexOrUppercaseWords instanceof RegExp) {
-        return strings.some((string) => regexOrUppercaseWords.test(string));
-    }
-    return strings.some((string) => regexOrUppercaseWords.every((word) => string.includes(word)));
-} 
+export function matchWords(codepoint: Codepoint, uppercaseWords: string[]): boolean {
+  return getCodepointUppercaseStrings(codepoint).some((string) =>
+    uppercaseWords.every((word) => string.includes(word))
+  )
+}
+
+export function matchRegexp(codepoint: Codepoint, regexp: RegExp): boolean {
+  return getCodepointUppercaseStrings(codepoint).some((string) => regexp.test(string))
+}
